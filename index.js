f5a351eabba3405243a76710f7d46698eff85eea
Emily Chew <emilychewsh@gmail.com>
2023-12-13 14:37:03 +1100

    completed lab

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  let ride;
+  if (feet <= 400){
+      return ride = "This one is on me!"
+  } else if (feet <= 2000){
+      return ride = "That will be twenty bucks."
+  } else if (feet < 2500){
+      return ride = "I will gladly take your thirty bucks."
+  } else if (feet > 2500){
+      return ride = "No can do."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+console.log(scuberGreetingForFeet(2600))
+
+
+function ternaryCheckCity(city){
+  return city === "NYC"? "Ok, sounds good." : "No go."
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+console.log(ternaryCheckCity("NYC"))
+
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case "generous":
+      return "Thank you so much."
+    case "not as generous":
+      return "Thank you."
+    case "thanks for everything":
+      return "Bye."
+  }
 }
+
+console.log(switchOnCharmFromTip("if anything else"))
